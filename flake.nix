0b91b9b2f1760fad3329b6eb7a57165718fcea10
Marc Jakobi <mrcjkb89@outlook.com>
2023-02-10 16:36:55 +0100

    add ci.yml

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     hakyll-flakes = {
       url = "github:Radvendii/hakyll-flakes";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils.url = "github:numtide/flake-utils";
     pre-commit-hooks = {
